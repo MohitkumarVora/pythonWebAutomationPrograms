e45d8c692e013e8cc2ed6a7164a8487bd0660167
Mohitkumar Vora <mohitv88@gmail.com>
2024-04-16 13:50:10 +0530

    create OrangeHRM Login script

---
+++ OrangeHRM/test_OrangeHRM.py
@@ -0,0 +1,37 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_OrangeHRM_Login():
+    driver = webdriver.Chrome()
+
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    driver.maximize_window()
+
+    time.sleep(5)
+
+    # Verify the Orange HRM Logo is Present
+    orangeHRM_Logo = driver.find_element(By.XPATH,"//img[@alt='company-branding']")
+    assert orangeHRM_Logo
+
+    # Get the Username
+    get_username = driver.find_element(By.XPATH, "//p[text()='Username : Admin']").text
+    get_username = get_username.replace("Username : ", "")
+
+    # Enter Username
+    driver.find_element(By.NAME, "username").send_keys(get_username)
+
+    # Get the Password
+    get_password = driver.find_element(By.XPATH, "//p[text()='Password : admin123']").text
+    get_password = get_password.replace("Password : ", "")
+
+    # Enter the Password
+    driver.find_element(By.NAME,"password").send_keys(get_password)
+
+    # Click on Login Button
+    driver.find_element(By.XPATH,"//button[@type='submit']").click()
+
+    # Verify the Change URL
+    assert driver.current_url == "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"

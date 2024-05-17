f42bdb9f08ec586c3548e701e5612e2ce1e5042f
Mohitkumar Vora <mohitv88@gmail.com>
2024-05-17 11:16:01 +0530

    update the comment text for better understanding

--- MiniProject4/test_miniproject4_excercise.py
+++ MiniProject4/test_miniproject4_excercise.py
@@ -19,7 +19,7 @@ def test_miniproject4():
     driver.find_element(By.ID, "gh-ac").send_keys("16 gb")
     # Click on Search Button
     driver.find_element(By.ID, "gh-btn").click()
-    #time.sleep(2)
+    # time.sleep(2)
 
     # Get the ITEM Name
     list_of_item_title_name = driver.find_elements(By.XPATH, "//span[@role='heading']")
@@ -35,14 +35,14 @@ def test_miniproject4():
     list_of_item_price = driver.find_elements(By.XPATH, "//span[@class='s-item__price']")
     prizes = []
 
-    # For loop will fetch all the top 60 Item price
+    # For loop will fetch all the top 60 Item prices
     for index, get_all_item_price in enumerate(list_of_item_price[1:62]):
         item_price = get_all_item_price.text
         print(index, "-", item_price)
 
         if "to" in item_price:  # get the first price when the price is having Range
 
-            # find the first price from the price range and move "$" sign and extra space
+            # find the first price from the price range and remove "$" sign and extra space
             get_only_value = item_price.split("to")[0].replace("$", "").strip()
             print("Split price is : ", get_only_value)  # print the first price value to verify
 
@@ -51,7 +51,7 @@ def test_miniproject4():
 
         else:  # get the price when item having single price
 
-            # store the price after move "$" sign and extra space
+            # store the price after remove "$" sign and extra space
             get_only_value = item_price.replace("$", "").strip()
 
             price_float = float(get_only_value)  # convert the price from "str" to "float"
